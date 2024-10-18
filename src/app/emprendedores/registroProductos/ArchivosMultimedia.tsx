ed7927f3d53784cd1f75cc7638f73fe108c9d254
Jean Villacis <jean.villacis@epn.edu.ec>
2024-10-18 09:26:17 -0500

    feat: Crear componente FormularioProducto para el ingreso de información del registro del producto.

---
+++ src/app/emprendedores/registroProductos/ArchivosMultimedia.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const ArchivosMultimedia = () => {
+  return (
+    <div>ArchivosMultimedia</div>
+  )
+}
--- src/app/emprendedores/registroProductos/FormularioProducto.tsx
+++ src/app/emprendedores/registroProductos/FormularioProducto.tsx
@@ -1,11 +1,7 @@
 "use client";
 import React, { useState } from 'react';
-import { Box, TextField, Typography, MenuItem } from '@mui/material';
+import { Box, TextField, Typography, MenuItem, Grid2 } from '@mui/material';
 import { themePalette } from '@/config/theme.config';
-
-interface Subcategories {
-  [key: string]: string[];
-}
 
 const productCategories = [
   { value: 'Comida', label: 'Comida' },
@@ -17,7 +13,7 @@ const productCategories = [
   { value: 'Servicios', label: 'Servicios' }
 ];
 
-const subcategories: Subcategories = {
+const subcategories = {
   Comida: ['Comida seca', 'Comida húmeda'],
   Correas: ['Nylon', 'Algodón', 'Cuero', 'Metálico'],
   Ropa: ['Chaquetas', 'Suéteres', 'Camisetas', 'Disfraces', 'Impermeables', 'Chalecos'],
@@ -30,194 +26,118 @@ const subcategories: Subcategories = {
 const sizeOptions = ['S', 'M', 'L', 'XL', 'XXL'];
 
 const FormularioRegistroProducto: React.FC = () => {
-  const [category, setCategory] = useState<string>('');
-  const [subcategory, setSubcategory] = useState<string>('');
-  const [size, setSize] = useState<string>(''); // Talla seleccionada
-  const [stock, setStock] = useState<string>('');
-  const [price, setPrice] = useState<string>(''); // Precio del producto
-  const [weight, setWeight] = useState<string>(''); // Peso para categoría de comida
-  const [description, setDescription] = useState<string>('');
+  const [formData, setFormData] = useState({
+    category: '',
+    subcategory: '',
+    size: '',
+    stock: '',
+    price: '',
+    weight: '',
+    description: ''
+  });
 
-  const handleCategoryChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setCategory(event.target.value);
-    setSubcategory(''); // Limpiar subcategoría al cambiar categoría
-    setWeight(''); // Limpiar el campo de peso si se cambia de categoría
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setFormData((prev) => ({ ...prev, [name]: value }));
   };
 
-  const handlePriceChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const value = e.target.value;
-    if (/^\d*\.?\d{0,2}$/.test(value)) {
-      setPrice(value);
-    }
-  };
-
-  return (
-    <Box
-      display="flex"
-      flexDirection="column"
-      alignItems="center" 
-      justifyContent="center"
-     
-      sx={{ padding: '20px', maxWidth: '1358px', margin: '0 auto', borderRadius: '10px', backgroundColor: themePalette.black10, boxShadow: 3, marginTop: '34px', color: themePalette.primary,  border: '1px solid #004040', }}
-    >
-      <Typography sx={{ color: themePalette.primary, width: '100%', fontSize: '24px', paddingLeft: '13px', fontWeight:'bold' }}>Información:</Typography>
-
-      <Box display="flex" flexDirection="column" alignItems="center" sx={{ width: '100%' }}>
-        {/* Nombre del producto */}
-        <Box display="flex" flexDirection="row" justifyContent="center" alignItems="center" sx={{ width: '600px' }}>
-          <Typography sx={{ width: '150px', textAlign: 'right', marginRight: '16px', fontSize:'24px', fontWeight:'600' }}>Nombre:</Typography>
-          <TextField
-            label="Ingresar"
-            margin="normal"
-            variant="outlined"
-            required
-            sx={{ flex: 1 }}
-          />
-        </Box>
-
-        {/* Categoría */}
-        <Box display="flex" flexDirection="row" justifyContent="center" alignItems="center" sx={{ width: '600px' }}>
-          <Typography sx={{ width: '150px', textAlign: 'right', marginRight: '16px', fontSize:'24px', fontWeight:'600'  }}>Categoría:</Typography>
+  const createField = (label: string, name: string, type = 'text', options?: string[], isSelect = false, textFieldProps: any = {}) => (
+    <>
+      <Grid2 
+      size={{xs:12,sm:4,lg:3}}
+      sx={{ display: 'flex', alignItems: 'center' }}>
+        <Typography sx={{ fontWeight: '600', fontSize: '18px', mb: '5px', minWidth: '50px' }}>{label}:</Typography>
+      </Grid2>
+      <Grid2 
+      size={{xs:12,sm:8,lg:9}}>
+      
+        {isSelect ? (
           <TextField
             select
             label="Seleccionar"
-            value={category}
-            onChange={handleCategoryChange}
+            name={name}
+            value={formData[name as keyof typeof formData]}
+            onChange={handleChange}
             fullWidth
             margin="normal"
             variant="outlined"
             required
-            sx={{ flex: 1 }}
+            sx={{ maxWidth: '400px' }}
+            {...textFieldProps}
           >
-            {productCategories.map((option) => (
-              <MenuItem key={option.value} value={option.value}>
-                {option.label}
+            {options?.map((option) => (
+              <MenuItem key={option} value={option}>
+                {option}
               </MenuItem>
             ))}
           </TextField>
-        </Box>
-
-        {/* Subcategoría */}
-        {category && (
-          <Box display="flex" flexDirection="row" justifyContent="center" alignItems="center" sx={{ width: '600px' }}>
-            <Typography sx={{ width: '150px', textAlign: 'right', marginRight: '16px', fontSize:'24px', fontWeight:'600'  }}>Subcategoría:</Typography>
-            <TextField
-              select
-              label="Seleccionar"
-              value={subcategory}
-              onChange={(e) => setSubcategory(e.target.value)}
-              fullWidth
-              margin="normal"
-              variant="outlined"
-              required
-              sx={{ flex: 1 }}
-            >
-              {subcategories[category]?.map((sub) => (
-                <MenuItem key={sub} value={sub}>
-                  {sub}
-                </MenuItem>
-              ))}
-            </TextField>
-          </Box>
-        )}
-
-        {/* Peso (solo para Comida) */}
-        {category === 'Comida' && (
-          <Box display="flex" flexDirection="row" justifyContent="center" alignItems="center" sx={{ width: '600px' }}>
-            <Typography sx={{ width: '150px', textAlign: 'right', marginRight: '16px' }}>Peso (kg):</Typography>
-            <TextField
-              label="Peso del producto (kg)"
-              value={weight}
-              onChange={(e) => setWeight(e.target.value)}
-              fullWidth
-              margin="normal"
-              variant="outlined"
-              required
-              type="number"
-              InputProps={{ inputProps: { min: 0, step: 0.01 } }}
-              sx={{ flex: 1 }}
-            />
-          </Box>
-        )}
-
-        {/* Stock */}
-        <Box display="flex" flexDirection="row" justifyContent="center" alignItems="center" sx={{ width: '600px' }}>
-          <Typography sx={{ width: '150px', textAlign: 'right', marginRight: '16px', fontSize:'24px', fontWeight:'600' }}>Stock:</Typography>
+        ) : (
           <TextField
             label="Ingresar"
-            value={stock}
-            onChange={(e) => {
-              const value = e.target.value;
-              if (/^\d*$/.test(value)) {
-                setStock(value);
-              }
-            }}
+            name={name}
+            value={formData[name as keyof typeof formData]}
+            onChange={handleChange}
             fullWidth
             margin="normal"
             variant="outlined"
             required
-            type="number"
-            InputProps={{ inputProps: { min: 0, step: 1 } }}
-            sx={{ flex: 1 }}
+            type={type}
+            sx={{ maxWidth: '400px' }}
+            {...textFieldProps}
           />
-        </Box>
-
-        {/* Precio sin IVA */}
-        <Box display="flex" flexDirection="row" justifyContent="center" alignItems="center" sx={{ width: '600px' }}>
-          <Typography sx={{ width: '150px', textAlign: 'right', marginRight: '16px', fontSize:'24px', fontWeight:'600' }}>Precio sin IVA:</Typography>
-          <TextField
-            label="Ingresar"
-            value={price}
-            onChange={handlePriceChange}
-            fullWidth
-            margin="normal"
-            variant="outlined"
-            required
-            type="text"
-            sx={{ flex: 1 }}
-          />
-        </Box>
-
-        {/* Talla */}
-        {(category === 'Ropa' || category === 'Correas' || category === 'Camas') && (
-          <Box display="flex" flexDirection="row" justifyContent="center" alignItems="center" sx={{ width: '600px' }}>
-            <Typography sx={{width: '150px', textAlign: 'right', marginRight: '16px', fontSize:'24px', fontWeight:'600' }}>Talla:</Typography>
-            <TextField
-              select
-              label="Seleccionar"
-              value={size}
-              onChange={(e) => setSize(e.target.value)}
-              fullWidth
-              margin="normal"
-              variant="outlined"
-              sx={{ flex: 1 }}
-            >
-              {sizeOptions.map((option) => (
-                <MenuItem key={option} value={option}>
-                  {option}
-                </MenuItem>
-              ))}
-            </TextField>
-          </Box>
         )}
+      </Grid2>
+    </>
+  );
 
-        {/* Descripción */}
-        <Box display="flex" flexDirection="row" justifyContent="center" alignItems="center" sx={{ width: '600px' }}>
-          <Typography sx={{width: '150px', textAlign: 'right', marginRight: '16px', fontSize:'24px', fontWeight:'600' }}>Descripción:</Typography>
-          <TextField
-            label="Ingresa información adicional"
-            value={description}
-            onChange={(e) => setDescription(e.target.value)}
-            fullWidth
-            margin="normal"
-            variant="outlined"
-            required
-            multiline
-            rows={4}
-            sx={{ flex: 1 }}
-          />
-        </Box>
-      </Box>
+  return (
+    <Box
+      sx={{
+        padding: '20px',
+        maxWidth: '1358px',
+        margin: '0 auto',
+        borderRadius: '10px',
+        backgroundColor: themePalette.black10,
+        boxShadow: 3,
+        marginTop: '34px',
+        color: themePalette.primary,
+        border: '1px solid #004040',
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center', // Centra horizontalmente
+        justifyContent: 'center', // Centra verticalmente
+        textAlign: 'center', // Asegura que el texto también esté alineado al centro
+      }}
+    >
+      <Typography
+        sx={{
+          color: themePalette.primary,
+          width: '100%',
+          fontSize: '24px',
+          paddingLeft: '13px',
+          fontWeight: 'bold',
+        }}
+      >
+        Información:
+      </Typography>
+      <Grid2
+        container
+        spacing={2}
+        sx={{
+          marginTop: '13px',
+          display: 'flex',
+          justifyContent: 'center',
+        }}
+      >
+        {createField('Nombre', 'name')}
+        {createField('Categoría', 'category', 'text', productCategories.map((cat) => cat.value), true)}
+        {formData.category && createField('Subcategoría', 'subcategory', 'text', subcategories[formData.category as keyof typeof subcategories], true)}
+        {formData.category === 'Comida' && createField('Peso (kg)', 'weight', 'number')}
+        {createField('Stock', 'stock', 'number')}
+        {createField('Precio sin IVA', 'price', 'number')}
+        {['Ropa', 'Correas', 'Camas'].includes(formData.category) && createField('Talla', 'size', 'text', sizeOptions, true)}
+        {createField('Descripción', 'description', 'text', undefined, false, { multiline: true, rows: 4, width: '700px' })}
+      </Grid2>
     </Box>
   );
 };
