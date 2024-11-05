c8514ab5d48df18625ac8fd2f142ffeeae5063cd
Jean Villacis <jean.villacis@epn.edu.ec>
2024-11-05 16:59:07 -0500

    feat: Crear componente para recuperar contraseña

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -1,8 +1,14 @@
+"use client";
+import { useState } from "react";
+import { Box, Grid2, Typography } from "@mui/material";
+import Image from "next/image";
+import { icon, fondoDuenos } from "@/assets/images";
+import PasswordRecovery from "./recuperarContraseña";
 
-export default function Home() {
-    return (
-      <div>
-        <h1>Login Page</h1>
-      </div>
-    );
-  }
+export default function Login() {
+  const [preferences, setPreferences] = useState(false);
+
+  return (
+   <PasswordRecovery />
+  );
+}
