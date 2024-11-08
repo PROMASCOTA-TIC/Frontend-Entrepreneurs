42e2e1f536f4c91e6b1874da731a52d58d999b31
Rafaela Robalino <rafaela.robalino@epn.edu.ec>
2024-11-08 16:30:01 -0500

    feat: Añadir Enlaces de interés

---
+++ src/validations/gestion-contenido/enviarEnlaceSchema.ts
@@ -0,0 +1,16 @@
+import { z } from 'zod';
+
+export const enviarEnlaceSchema = z.object({
+    categoria: z
+        .string()
+        .min(1, { message: 'La categoría es obligatoria', }),
+    titulo: z
+        .string()
+        .min(1, { message: 'El título es obligatorio', }),
+    descripcion: z
+        .string()
+        .min(1, { message: 'La descripción es obligatoria', }),
+    fuentes: z
+        .string()
+        .min(1, { message: 'La fuente es obligatoria', }),
+})
