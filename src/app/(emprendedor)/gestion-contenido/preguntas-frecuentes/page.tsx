1f5462fc26518a25433a7236b1f377185ce9822e
Rafaela Robalino <rafaela.robalino@epn.edu.ec>
2025-01-08 17:38:09 -0500

    feat: Correciones en Preguntas Frecuentes

--- src/app/(emprendedor)/gestion-contenido/preguntas-frecuentes/page.tsx
+++ src/app/(emprendedor)/gestion-contenido/preguntas-frecuentes/page.tsx
@@ -1,10 +1,17 @@
 import BarraDeBusqueda from "@/components/gestionContenido/BarraBusqueda";
 import PF_Categorias from "./PF_Categorias";
 
-export default function EnlacesDeInteres() {
+import "/src/assets/styles/gestionContenido/general.css";
+import "/src/assets/styles/gestionContenido/estilos.css";
+
+export default function PreguntasFrecuente() {
     return (
         <div>
-            <BarraDeBusqueda />
+            <BarraDeBusqueda
+                endpoint="http://localhost:3001/api/faqs/search"
+                seccion="preguntas-frecuentes"
+                placeholder="Buscar preguntas frecuentes"
+            />
             <h1 className="h1-bold txtcolor-primary flex-center" style={{ padding: '21px 0px 0px 0px' }}>Categorías</h1>
             <PF_Categorias />
         </div>
